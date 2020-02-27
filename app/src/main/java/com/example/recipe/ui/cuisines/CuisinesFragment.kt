678ae56e74b615b45c7d8d38271c51ffa0164ef0
Albert Yu <albertyu_85@yahoo.com>
2020-02-26 19:49:39 -0800

    added directions button to open web browser

--- app/src/main/java/com/example/recipe/ui/cuisines/CuisinesFragment.kt
+++ app/src/main/java/com/example/recipe/ui/cuisines/CuisinesFragment.kt
@@ -6,10 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recipe.R
+import com.example.recipe.databinding.CuisinesFragmentBinding
 import com.example.recipe.ui.detail.DetailFragment
 import kotlinx.android.synthetic.main.cuisines_fragment.*
 
@@ -21,17 +23,20 @@ class CuisinesFragment : Fragment() {
     }
 
     private lateinit var viewModel: CuisinesViewModel
-
+    private lateinit var binding: CuisinesFragmentBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
 
-        return inflater.inflate(R.layout.cuisines_fragment, container, false)
+        val view = inflater.inflate(R.layout.cuisines_fragment, container, false)
+        binding = DataBindingUtil.inflate<CuisinesFragmentBinding>(inflater, R.layout.cuisines_fragment, container, false)
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
         viewModel = ViewModelProviders.of(this).get(CuisinesViewModel::class.java)
         val adapter = CuisinesAdapter {cuisineTitle : String -> onClick(cuisineTitle)}
         adapter.data = viewModel.data
--- app/src/main/java/com/example/recipe/ui/directions/DirectionsFragment.kt
+++ app/src/main/java/com/example/recipe/ui/directions/DirectionsFragment.kt
@@ -1,5 +1,7 @@
 package com.example.recipe.ui.directions
 
+import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +9,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,6 +17,7 @@ import com.bumptech.glide.Glide
 
 import com.example.recipe.R
 import com.example.recipe.databinding.DirectionsFragmentBinding
+import com.example.recipe.model.RecipeInformation
 import kotlinx.android.synthetic.main.directions_fragment.*
 
 class DirectionsFragment : Fragment() {
@@ -25,9 +29,11 @@ class DirectionsFragment : Fragment() {
     ): View? {
 
         val args = DirectionsFragmentArgs.fromBundle(arguments!!)
-        val binding : DirectionsFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.directions_fragment, container, false)
+        val binding: DirectionsFragmentBinding =
+            DataBindingUtil.inflate(inflater, R.layout.directions_fragment, container, false)
         val view = binding.root
-        viewModel = ViewModelProviders.of(this,
+        viewModel = ViewModelProviders.of(
+            this,
             DirectionsViewModelFactory(args.recipeID)
         ).get(DirectionsViewModel::class.java)
         viewModel.getRecipeInformation()
@@ -41,7 +47,16 @@ class DirectionsFragment : Fragment() {
             Log.d("Directions Fragment", "${it.extendedIngredients}")
             binding.recyclerViewIngredientsList.adapter = adapter
         })
-
+        val buttonDirections = view.findViewById<Button>(R.id.button_directions)
+        buttonDirections.setOnClickListener {
+            openDirections(viewModel.recipeInfo.value)
+        }
         return view
+    }
+
+    fun openDirections(recipeInformation: RecipeInformation?) {
+        val openURL = Intent(android.content.Intent.ACTION_VIEW)
+        openURL.data = Uri.parse(recipeInformation?.sourceUrl)
+        startActivity(openURL)
     }
 }
